470cc9f8240d097c8042da9188d81369d86f0ab9
Roey Feingold- yoga 9 <roey.feingold@gmail.com>
2021-11-06 21:14:54 +0200

    Update isArmstring & myPow & countDigitNum after main check

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -1,20 +1,20 @@
 #include "NumClass.h"
 
 //Loop
-int pow(int a, int b);
+int myPow(int a, int b);
+int countDigitNum(int n);
 
 /* will return if a number is Armstrong number
 An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
 For Example: 407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
 */
 int isArmstrong(int n){
-    int count=0, sum=0;
+    int sum=0;
+    int count=countDigitNum(n);
     if (n==0){
         return 1;
     }else{    
-        for (int i = n; i!=0; ++count){ // counts the  num of digits of n
-            i=i/10;
-        }
+
         for (int N = n; N != 0 ; N=N/10){
             int mod=N%10;
             sum=sum+myPow(mod,count);
@@ -32,10 +32,18 @@ int isPalindrome(int n){
 
 }
 
-int pow(int a, int b){
+int myPow(int a, int b){
     int Pow=1;
     for (int i = 0; i < b; i++){
-        Pow=Pow*Pow;
+        Pow=Pow*a;
     }
-    
+    return Pow;
+}
+
+int countDigitNum(int n){
+    int count=0;
+    for (int i = n; i!=0; ++count){ // counts the  num of digits of n
+            i=i/10;
+        }
+    return count;
 }
