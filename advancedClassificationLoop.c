08dbe1b1a4670719efd586030759dbc588b07424
Roey Feingold- yoga 9 <roey.feingold@gmail.com>
2021-11-06 19:44:46 +0200

    Update isArmstrong & isPalindrome functions

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -6,10 +6,10 @@
 An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
 For Example: 407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
 */
-int isArmstrong(int){
-    return 0;
+int isArmstrong(int n){
+    
 }
 /* will return if a number is a palindrome */
-int isPalindrome(int){
+int isPalindrome(int n){
 
 }
