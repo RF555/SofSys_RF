5468c80dbe3e1ca9da80eb6d19deaf959d4d3eb2
Roey Feingold- yoga 9 <roey.feingold@gmail.com>
2021-11-22 13:08:31 +0200

    Update main.c functions

--- main.c
+++ main.c
@@ -3,14 +3,16 @@
 
 #define N 10
 #define T 2
-#define DIS 2147483647;
-#define Z 0;
-#define MIN_NUM -2147483648;
+#define DIS 0
+#define Z 0
+#define MAX_NUM 2147483648
+#define MIN_NUM -2147483648
 
 // remember pointers and memory usage of arrays
 
+// because the graph is undirected: mat[i][j]=mat[j][i]
 void init_mat(int mat[N][N]){
-    for(int i=0;i<N;i++){
+    for(int i=Z;i<N;i++){
         mat[i][i]=Z;
         for(int j=i+1; j<N;j++){
             mat[i][j]=DIS;
@@ -19,9 +21,9 @@ void init_mat(int mat[N][N]){
     }
 }
 
-int return_init_mat(mat[N][N]){
-    int *p; // initiate pointer
-    return
+int return_init_mat_0_0(int mat[N][N]){
+    int p=&mat[Z][Z];
+    return p;
 }
 
 int main(){
