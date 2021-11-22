cfafd4e322c87124ffb65fbcb72762db0364623b
Roey Feingold- yoga 9 <roey.feingold@gmail.com>
2021-11-22 11:08:20 +0200

    Create init_mat function

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// #include "NumClass.h"
+
+#define N 10
+#define T 2
+#define DIS 2147483647;
+#define Z 0;
+#define MIN_NUM -2147483648;
+
+// remember pointers and memory usage of arrays
+
+void init_mat(int mat[N][N]){
+    for(int i=0;i<N;i++){
+        mat[i][i]=Z;
+        for(int j=i+1; j<N;j++){
+            mat[i][j]=DIS;
+            mat[j][i]=DIS;            
+        }
+    }
+}
+
+int main(){
+    int mat[N][N];
+    init_mat(mat);
+
+    return 0;
+}
