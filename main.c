2d915df1d9fa2fa99ee96dbc28f8d09023eeb13e
Roey Feingold- yoga 9 <roey.feingold@gmail.com>
2021-11-22 20:52:59 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-// #include "NumClass.h"
+#include "my_mat.h"
 
 #define N 10
 #define T 2
@@ -12,6 +12,40 @@ int main()
 {
     int mat[N][N];
     init_mat(mat);
-
+    char select = 'E';
+    int first = 0;
+    while (select != 'D')
+    {
+        printf("Enter A, B, C or D:\n");
+        scanf("%c", select);
+        if (select == 'A')
+        {
+            /* code */
+            func_A(mat);
+            ++first;
+        }
+        else if (select == 'B')
+        {
+            /* code */
+            if (first == 0)
+            {
+                /* code */
+                printf("Matrix not initialized yet! please type A!");
+            }
+            else
+            {
+                func_B(mat);
+            }
+        }
+        else if (select == 'C')
+        {
+            /* code */
+        }
+        else if (select == 'D')
+        {
+            break;
+        }
+    }
+    printf("Bye-Bye!\n");
     return 0;
 }
