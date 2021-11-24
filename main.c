e83c012955d152158e2f4e15fce4e5f5659e8088
Roey Feingold- yoga 9 <roey.feingold@gmail.com>
2021-11-24 14:55:28 +0200

    Corrected files

--- main.c
+++ main.c
@@ -3,6 +3,68 @@
 
 int main()
 {
-
+    int mat[N][N], path_mat[N][N];
+    init_mat(&mat[0][0], &path_mat[0][0]);
+    char select = 'E';
+    char *sel_p = &select;
+    int firstA = Z;
+    int firstB = Z;
+    int firstC = Z;
+    while (select != 'D')
+    {
+        // printf("Enter A, B, C or D:\n");
+        scanf("%c", sel_p);
+        if (*sel_p == 'A')
+        {
+            firstA = INIT;
+            firstB = INIT;
+            firstC = INIT;
+            // Function A
+            func_A(&mat[0][0], &path_mat[0][0]);
+            *sel_p = 'E';
+        }
+        // Function B
+        else if (*sel_p == 'B')
+        {
+            if (firstA == Z)
+            {
+                printf("Matrix not initialized yet! please type A!");
+            }
+            else if (firstB != Z || firstC != Z)
+            {
+                bool_B(&path_mat[0][0]);
+            }
+            else
+            {
+                ++firstB;
+                func_B(&mat[0][0], &path_mat[0][0]);
+            }
+            *sel_p = 'E';
+        }
+        // Function C
+        else if (*sel_p == 'C')
+        {
+            if (firstA == Z)
+            {
+                // printf("Matrix not initialized yet! please type A!");
+            }
+            else if (firstC != Z)
+            {
+                ++firstC;
+                prev_C(&path_mat[0][0]);
+            }
+            else
+            {
+                ++firstC;
+                func_C(&mat[0][0], &path_mat[0][0]);
+            }
+            *sel_p = 'E';
+        }
+        else if (*sel_p == 'D') // select == 'D'
+        {
+            // printf("Bye-Bye!\n");
+            break;
+        }
+    }
     return 0;
 }
--- my_mat.h
+++ my_mat.h
@@ -1,6 +1,26 @@
 #ifndef _MY_MAT_H_
 #define _MY_MAT_H_
 
+#define N 10
+#define TRUE 1
+#define FALSE 0
+#define DIS 0
+#define Z 0
+#define INIT 1
+#define MAX_NUM 2147483648
+#define MIN_NUM -2147483648
+#define ERR -1
 
+int init_mat(int *mat, int *path_mat);
+// FUNCTION A: Initialize and generate a matrix from input
+int func_A(int *mat, int *path_mat);
+
+// FUNCTION B: Prints "True" if there's a path from i to j, else- prints "False"
+int bool_B(int *path_mat);
+int func_B(int *mat, int *path_mat);
+
+// FUNCTION C: Prints the shortest path from i to j
+int prev_C(int *path_mat);
+int func_C(int *mat, int *path_mat);
 
 #endif
