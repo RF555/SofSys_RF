93bce09690f9482bce69bc5bf5abbe5b769267a6
Roey Feingold- yoga 9 <roey.feingold@gmail.com>
2021-12-14 11:11:03 +0200

    Update my_seq.h

---
+++ my_seq.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <my_seq.h>
+
+int gematria(char (*word)[WORD], int (*text)[TXT]);
+
+int gematria_seq(char (*word)[WORD], int (*text)[TXT]){
+
+
+    return 0;
+}
+
+
+
+
+
+
+int atbash_seq(char (*word)[WORD], int (*text)[TXT]){
+
+
+    // return 0;
+}
+
+
+
+
+
+
+int anagram_seq(char (*word)[WORD], int (*text)[TXT]){
+
+
+    // return 0;
+}
+
--- my_seq.h
+++ my_seq.h
@@ -3,11 +3,14 @@
 
 #define TXT 1024
 #define WORD 30
+#define GEM "Gematria Sequences:"
+#define ATB "Atbash Sequences:"
+#define ANG "Anagram Sequences:"
 
-char gematria(char (*word)[WORD], int (*text)[TXT]);
+int gematria_seq(char (*word)[WORD], int (*text)[TXT]);
 
+int atbash_seq(char (*word)[WORD], int (*text)[TXT]);
 
-
-
+int anagram_seq(char (*word)[WORD], int (*text)[TXT]);
 
 #endif
