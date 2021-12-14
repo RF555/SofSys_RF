6fe7d8737d9e0475a361aaa76d545c69d0eba1e2
Roey Feingold- yoga 9 <roey.feingold@gmail.com>
2021-12-14 11:20:45 +0200

    Update my_seq.h

--- my_seq.h
+++ my_seq.h
@@ -3,9 +3,18 @@
 
 #define TXT 1024
 #define WORD 30
+#define WE1 ' '
+#define WE2 '\t'
+#define WE3 '\n'
+#define TXTE '~'
 #define GEM "Gematria Sequences:"
 #define ATB "Atbash Sequences:"
 #define ANG "Anagram Sequences:"
+#define A_ASCII 65
+#define Z_ASCII 90
+#define a_ASCII 97
+#define z_ASCII 122
+#define LETT_NUM 26
 
 int gematria_seq(char (*word)[WORD], int (*text)[TXT]);
 
